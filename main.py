5c6566a6dd68d132bbaa9d09499eb2b42746ffb1
Stephan Delhey <stephan@Stephans-MacBook-Pro.local>
2019-02-10 18:52:54 +0100

    checking for actual majority (X>N/2)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-data = ["red","green","red","red","green"]
+data = ["green","blue","red","red","green"]
 
 def freq(arr, item):
     counter = 0
@@ -6,6 +6,14 @@ def freq(arr, item):
         if (arr[i] == item):
             counter += 1
     return counter
+
+def findMajority(arr):
+    result = findMajorityElementDivAndConq(arr)
+    counter = freq(arr, result)
+    if (counter > len(arr) / 2):
+        return result
+    else:
+        return "NO MAJORITY"
 
 def findMajorityElementDivAndConq(arr):
 
@@ -53,4 +61,7 @@ def findMajorityElementDivAndConq(arr):
             print("right")
             return right
 
-print(findMajorityElementDivAndConq(data))
+
+result = findMajority(data)
+print("----")
+print(result)
